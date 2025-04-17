69152d760d50165a7a6a6a6f67c2d6c1a2eef262
petrikatase <75438845+petrikatase@users.noreply.github.com>
2025-04-17 14:45:41 +0200

    Update slides.js

--- fotos/slides.js
+++ fotos/slides.js
@@ -1,6 +1,3 @@
-const basePath = window.location.origin + window.location.pathname.replace(/\/[^\/]*$/, '/') + 'fotos/';
-window.imageList = ['slide1.jpg', 'slide2.jpg', 'slide3.jpg'].map(name => basePath + name);
-
 window.imageList = [
     './fotos/slide1.jpg',
     './fotos/slide2.jpg',
