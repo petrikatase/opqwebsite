4bca9cbbce038200542465387993187f81858d08
petrikatase <75438845+petrikatase@users.noreply.github.com>
2025-04-17 14:31:12 +0200

    Update slides.js

--- fotos/slides.js
+++ fotos/slides.js
@@ -1,9 +1,12 @@
+const basePath = window.location.origin + window.location.pathname.replace(/\/[^\/]*$/, '/') + 'fotos/';
+window.imageList = ['slide1.jpg', 'slide2.jpg', 'slide3.jpg'].map(name => basePath + name);
+
 window.imageList = [
-    'fotos/slide1.jpg',
-    'fotos/slide2.jpg',
-    'fotos/slide3.jpg',
-    'fotos/slide4.jpg',
-    'fotos/slide5.jpg',
-    'fotos/slide6.jpg'
+    '/fotos/slide1.jpg',
+    '/fotos/slide2.jpg',
+    '/fotos/slide3.jpg',
+    '/fotos/slide4.jpg',
+    '/fotos/slide5.jpg',
+    '/fotos/slide6.jpg'
   ];
   
