ddb900f1b26954eecc91c0d41ee13037a23a7af5
petrikatase <75438845+petrikatase@users.noreply.github.com>
2025-04-17 14:25:31 +0200

    Update slides.js

--- fotos/slides.js
+++ fotos/slides.js
@@ -1,9 +1,9 @@
 window.imageList = [
-    'slide1.jpg',
-    'slide2.jpg',
-    'slide3.jpg',
-    'slide4.jpg',
-    'slide5.jpg',
-    'slide6.jpg'
+    'fotos/slide1.jpg',
+    'fotos/slide2.jpg',
+    'fotos/slide3.jpg',
+    'fotos/slide4.jpg',
+    'fotos/slide5.jpg',
+    'fotos/slide6.jpg'
   ];
   
